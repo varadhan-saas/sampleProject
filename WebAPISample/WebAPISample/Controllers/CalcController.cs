ebc03da92239cd66c78f3be7f3c19699bc695583
Varadhan <varadhan3n90@gmail.com>
2014-09-08 22:12:27 +0530

    Added web api project and enabled CORS

---
+++ WebAPISample/WebAPISample/Controllers/CalcController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAPISample.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CalcController : ApiController
+    {
+        [HttpGet]
+
+        public int Calculator([FromUri]int a,[FromUri]int b,[FromUri]string op)
+        {
+            if (op.Equals("add"))
+                return a + b;
+            if(op.Equals("sub"))
+                return a-b;
+            return 0;
+        }
+
+    }
+}
